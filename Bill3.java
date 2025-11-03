1ff9507f11092911f6650a047a464aa244886dc9
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-11-03 11:51:33 +0200

    Hw1 - check

--- Bill3.java
+++ Bill3.java
@@ -2,15 +2,13 @@
 public class Bill3 {
 	public static void main(String[] args) {
 		
-	     {
-String name1 = args[0];
-String name2 = args[1];
-String name3 = args[2];
-int bill = Integer.parseInt(args[3]);
-double ResultBill = (double) bill/  3.0;
-System.out.println("dear " + name3 + " " + name2 + " " + name1 + " you need to pay: " + ResultBill);
-}
+	     
+	String name1 = args[0];
+	String name2 = args[1];
+	String name3 = args[2];
+	int bill = Integer.parseInt(args[3]);
+	double ResultBill = (double) bill/  3.0;
+	System.out.println("dear " + name3 + " " + name2 + " " + name1 + " you need to pay: " + Math.ceil(ResultBill));
 
- 
 	}
 }
