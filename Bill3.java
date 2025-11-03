4364d0aa0a0c102f8688ea8e52c6e70cf4d6d463
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-11-03 12:10:03 +0200

    Hw1 - fix bill3

--- Bill3.java
+++ Bill3.java
@@ -7,8 +7,8 @@ public class Bill3 {
 	String name2 = args[1];
 	String name3 = args[2];
 	int bill = Integer.parseInt(args[3]);
-	double ResultBill = (double) bill/  3.0;
-	System.out.println("dear " + name3 + " " + name2 + " " + name1 + " you need to pay: " + Math.ceil(ResultBill));
+	double ResultBill =  Math.ceil(bill/3.0);
+	System.out.println("Dear " + name3 + ", " + name2 + ", and " + name1 + ": pay " + ResultBill + " shekels each.");
 
 	}
 }
