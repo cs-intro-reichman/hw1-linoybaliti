46dc69735ced37f4518b47bbc27008fe7b2d4338
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-11-03 15:40:29 +0200

    Hw1 - fix numw3

--- NumWords.java
+++ NumWords.java
@@ -6,7 +6,7 @@ int num1 = Integer.parseInt(args[0]);
 int hundred =  (num1 / 100 ) %10;
 int tens =  ( num1 / 10 ) %10;
 int ones =  num1 % 10;
-System.out.println( hundred + " hundreds, " + tens + " tens, " + ones + " ones. " );
+System.out.println( hundred + " hundreds, " + tens + " tens, and " + ones + " ones." );
 
 
 	}
