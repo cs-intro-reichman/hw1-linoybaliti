f62c699d19cae6cfd508e3d68edf46b54666f38b
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-11-03 12:35:24 +0200

    Hw1 - fix FVC

--- FVCalc.java
+++ FVCalc.java
@@ -6,7 +6,7 @@ int sum = Integer.parseInt(args[0]); // סכום השקעה ראשוני
 double rate = Double.parseDouble(args[1])/100; // אחוז ריבית
 int n =Integer.parseInt(args[2]); // מספר שנים בהשקעה
 double FutureValue = sum*(Math.pow(rate +1 ,n));
-System.out.println("after " + n + " years " + sum + "$,saved at " + rate*100 + "% will yield " + (int)FutureValue + "$");
+System.out.println("After " + n + " years, $" + sum + " saved at " + rate*100 + "% will yield $" + (int)FutureValue );
 
 
 	}
