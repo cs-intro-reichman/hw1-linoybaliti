13b6ef40d0a8427010fa0344b29359624f4f7eff
Linoy Baliti <linoy.baliti@post.runi.ac.il>
2025-11-04 11:33:33 +0200

    Hw1 - fix NumWords

--- NumWords.java
+++ NumWords.java
@@ -3,10 +3,10 @@ public class NumWords {
 	public static void main(String args[]) {
 
 int num1 = Integer.parseInt(args[0]);
-int hundred =  (num1 / 100 ) %10;
-int tens =  ( num1 / 10 ) %10;
-int ones =  num1 % 10;
-System.out.println( hundred + " hundreds, " + tens + " tens, and " + ones + " ones." );
+int hundred =  (num1/100);
+int tens =  (num1/10)%10;
+int ones =  num1%10;
+System.out.println(hundred + " hundreds, " + tens + " tens, and " + ones + " ones." );
 
 
 	}
