80376215d9d47eaf0dd6037d0d1169e64a94ad27
YamlRUNI <155106022+YamlRUNI@users.noreply.github.com>
2025-11-10 23:16:21 +0200

    Feedback Ascend.java

--- Ascend.java
+++ Ascend.java
@@ -1,3 +1,16 @@
+/*
+
+feedback:
+
+Great Work
+
+Yam
+
+*/
+
+
+
+
 // Generates three integer random numbers in a given range,
 // and prints them in increasing order.
 public class Ascend {
@@ -18,3 +31,4 @@ System.out.println(Min + " " + Mid + " " + Max);
 
 	}
 }
+
